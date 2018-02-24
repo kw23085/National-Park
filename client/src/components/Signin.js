74f015e36f975dc8a12e7898e3bb62a14ed4ad57
Ishan <ishanliou@yahoo.com>
2018-02-23 18:25:53 -0800

    sign in sign up form disply only

--- client/src/components/Signin.js
+++ client/src/components/Signin.js
@@ -3,7 +3,23 @@ class Signin extends Component{
 
     render(){
         return(
-            <div>Sign in!</div>
+            <div>
+                <h1>Sign in!</h1>
+                    <form>
+                        <div>
+                        <label>Email:</label>
+                        <input type="text" ref="email" placeholder="Email"/>
+                        </div>
+                        <div>
+                        <label>Password:</label>
+                        <input type="text" ref="password" placeholder="Password"/>
+                        <button type="button" className="btn btn-info" >Sign in</button>
+                        </div>
+                    </form>
+
+
+
+            </div>
         )
     }
 }
--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -3,7 +3,31 @@ import React, { Component } from 'react';
 class Signup extends Component{
     render(){
         return(
-            <div>Sign up!</div>
+            <div>
+                <h1>Sign UP</h1>
+                <form>
+                    <div>
+                    <label>Name:</label>
+                    <input type="text" ref="name" placeholder="Name"/>
+                    </div>
+
+                    <div>
+                    <label>Email:</label>
+                    <input type="text" ref="email" placeholder="Email"/>
+                    </div>
+
+                    <div>
+                    <label>Password:</label>
+                    <input type="text" ref="password" placeholder="Password"/>
+                    <button type="button" className="btn btn-info" >Sign in</button>
+                    </div>
+                </form>
+
+
+
+
+
+            </div>
         )
     }
 }
