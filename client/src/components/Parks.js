66cd79f93e887be7e6b1921a6bf39f89a06af737
Ishan <ishanliou@yahoo.com>
2018-02-24 18:45:01 -0800

    disply parks

--- client/src/components/Parks.js
+++ client/src/components/Parks.js
@@ -1,17 +1,35 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 class Parks extends Component {
+    state = {
+        filterString : '',
+        parks: this.props.parks.data
+    }
 
+    updateFilter(){
+        this.setState({
+            filterString: this.refs.filterinput.value
+        })
+    }
 
     render(){
+        const { filterString,parks } = this.state
+        const parklist = parks.filter((p) => {
+            return p.fullName.toLowerCase().includes(filterString.toLowerCase())
+          })
+        // console.log("Parks props: ", this.props)
         return(
             <div>
                 <h1> Hello! National Parks </h1>
                 <h1>Hi</h1>
                 <h3>
-                    <ul>{this.props.parks.data && this.props.parks.data.map((p, i)=>{
+                    <input onChange={this.updateFilter.bind(this)}ref="filterinput" type="text" placeholder="Search a Park"/>
+                    <ul>{parklist && 
+                        parklist.map((p, i)=>{
                         return p.designation === "National Park" ? 
-                            <li key={i}> {p.fullName} 
+                            <li key={i}> 
+                            <Link to=""> {p.fullName}  </Link>
                                 {/* <p>{p.description}</p> */}
                             </li>
                             : null
@@ -19,6 +37,7 @@ class Parks extends Component {
                     </ul>
                 
                 </h3>
+                
             </div>  
         )
     }
