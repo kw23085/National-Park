a1c6d84aaf5ebe04e6b444527bb9f8b01d27d2b0
Kevin Wang <kw1017@gmail.com>
2018-06-28 15:30:23 -0700

    hero img fix

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -4,9 +4,16 @@ import React, { Component } from 'react'
 class Home extends Component {
     render(){
         return(
-            <div className="heroImg"> 
-                <img style={{width:'100%', height:'auto'}} alt="" src={`/images/hero-Yosemite.jpg`}/>   
-                <div className="centerText">Pocket Park</div>
+            <div className="wrapper"> 
+                <img className="hero-img" alt="heroimage" src={`/images/hero1.jpeg`}/>   
+                <div className="centerText">
+                    <div className="hero-small-text">The</div>
+                    <div className="hero-large-text">World</div>
+                    <div className="hero-small-text">Is</div>
+                    <div className="hero-large-text">Yours</div>
+                    <div className="hero-small-text">To</div>
+                    <div className="hero-large-text">Explore</div>
+                </div>
                 <div className="credit">Special Thanks to <a href="https://www.nps.gov/index.htm" target="_blank">National Park Service</a><img className="nps-logo" alt="" src={`/images/nps-logo.png`} /></div>
             </div>
         )
